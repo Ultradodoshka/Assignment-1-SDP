092976094a767ed65f5a85c8e1ed21e968da0be7
Ultradodoshka <super.duper.dodo00@gmail.com>
2026-09-13 12:50:33 +0500

    Added Client

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,6 +1,18 @@
-
 public class Main {
     public static void main(String[] args) {
+        Director director = new Director();
+        Builder car1 = new CityCar();
 
+        director.constructToyota(car1);
+        Car toyota = car1.build();
+        System.out.println(toyota);
+
+        Car tesla = car1
+                .setName("Tesla")
+                .setColor("white")
+                .setSpeed(100)
+                .setFuelCapacity(80)
+                .build();
+        System.out.println(tesla);
     }
 }
